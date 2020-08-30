44eb01981233b3daf63f89a5de19cd3286305dcc
Wondun <49043385+Wondun@users.noreply.github.com>
2020-08-30 17:06:55 +0200

    del

---
+++ delete.php
@@ -0,0 +1,17 @@
+<?php
+session_start();
+$user_id = $_SESSION['id'];
+$title = $_POST['title'];
+$text = $_POST['text'];
+
+require_once "connect.php";
+
+	$polaczenie = @new mysqli($host, $db_user, $db_password, $db_name);
+    
+    $delete = mysqli_query($polaczenie,"DELETE (`id`, `title`, `text`, `user_id`) FROM `notatki` WHERE title='$title';
+    
+    mysqli_close($polaczenie);
+    header('Location: gra.php');
+
+
+?>
