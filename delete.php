8598600833a303693c65f88fbd9e154787347be0
Wondun <49043385+Wondun@users.noreply.github.com>
2020-08-30 18:01:32 +0200

    update

--- delete.php
+++ delete.php
@@ -1,12 +1,12 @@
 <?php
 session_start();
-$id = $_POST["id"]
+$id = $_POST["id"];
 
 require_once "connect.php";
 
 	$polaczenie = @new mysqli($host, $db_user, $db_password, $db_name);
     
-    $delete = mysqli_query($polaczenie,"DELETE FROM `notatki` WHERE id='$id');
+    $delete = mysqli_query($polaczenie,"DELETE FROM `notatki` WHERE id='$id'");
     
     mysqli_close($polaczenie);
     header('Location: gra.php');
