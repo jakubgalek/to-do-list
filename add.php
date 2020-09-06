f0b2b9e6406bd2c79ef1ce87f6cd578b6bf9f6c9
Wondun <49043385+Wondun@users.noreply.github.com>
2020-09-06 18:31:05 +0200

    fixed images

--- add.php
+++ add.php
@@ -14,7 +14,7 @@ require_once "connect.php";
 
     
 
-    $target_dir = "";
+    $target_dir = "uploads/";
     $target_file = $target_dir . basename($_FILES["fileToUpload"]["name"]);
     $uploadOk = 1;
     $imageFileType = strtolower(pathinfo($target_file,PATHINFO_EXTENSION));
@@ -55,7 +55,6 @@ require_once "connect.php";
     echo "Sorry, your file was not uploaded.";
   // if everything is ok, try to upload file
   } else {
-    $target_dir = "uploads/";
     if (move_uploaded_file($_FILES["fileToUpload"]["tmp_name"], $target_file)) {
       echo "The file ". basename( $_FILES["fileToUpload"]["name"]). " has been uploaded.";
     } else {
